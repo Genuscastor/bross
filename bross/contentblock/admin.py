c84f7877555b14705e4d9cf9f1355dcc2a37b9d0
RickDoesburg <rickdoesburg@gmail.com>
2013-05-28 14:42:09 +0200

    Admin.py added

---
+++ bross/contentblock/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from bross.contentblock.models import Publisher, Author, Book
+
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Book)
