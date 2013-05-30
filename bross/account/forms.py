d32ef8cd4dc6d10b75dc5406ccd666b0c398909a
RickDoesburg <rickdoesburg@gmail.com>
2013-05-30 14:13:00 +0200

    Updates form tut. Still not working

--- bross/account/forms.py
+++ bross/account/forms.py
@@ -4,27 +4,29 @@ from django.forms import ModelForm
 from bross.account.models import Account
 
 class RegistrationForm(ModelForm):
-	username 	= forms.CharField(label=(u'User Name'))
-	email 		= forms.EmailField(label=(u'Email Adress'))
-	password 	= forms.CharField(label=(u'Password'), widget=forms.PasswordInput(render_value=False))
-	password1 	= forms.CharField(label=(u'Verify Password'), widget=forms.PasswordInput(render_value=False))
+        username        = forms.CharField(label=(u'User Name'))
+        email           = forms.EmailField(label=(u'Email Address'))
+        password        = forms.CharField(label=(u'Password'), widget=forms.PasswordInput(render_value=False))
+        password1       = forms.CharField(label=(u'Verify Password'), widget=forms.PasswordInput(render_value=False))
 
-	class Meta:
-		model = Account
-		exclude = ('user',)
+        class Meta:
+                model = Account
+                exclude = ('user',)
 
         def clean_username(self):
                 username = self.cleaned_data['username']
                 try:
                         User.objects.get(username=username)
                 except User.DoesNotExist:
-                	return username
+                        return username
                 raise forms.ValidationError("That username is already taken, please select another.")
 
-        # def clean(self):
-        #         if self.cleaned_data['password'] != self.cleaned_data['password1']:
-        #         	#TODO: FIXEN ERROR MELDING
-        #         	raise forms.ValidationError("The passwords did not match.  Please try again.")
-        #         return self.cleaned_data
+        def clean(self):
+                if self.cleaned_data['password'] != self.cleaned_data['password1']:
+                        raise forms.ValidationError("The passwords did not match.  Please try again.")
+                return self.cleaned_data
 
-		return self.cleaned_data
+class LoginForm(forms.Form):
+        username        = forms.CharField(label=(u'User Name'))
+        password        = forms.CharField(label=(u'Password'), widget=forms.PasswordInput(render_value=False))
+		
--- bross/account/views.py
+++ bross/account/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from bross.account.forms import RegistrationForm
+from bross.account.forms import RegistrationForm, LoginForm
 from bross.account.models import Account
+from django.contrib.auth import authenticate, login, logout
 
 def AccountRegistration(request):
         if request.user.is_authenticated():
@@ -11,9 +13,9 @@ def AccountRegistration(request):
         if request.method == 'POST':
                 form = RegistrationForm(request.POST)
                 if form.is_valid():
-                        user = User.objects.create_user(username=form.cleaned_data['username'], email =form.cleaned_data['email'], password = form.cleaned_data['password'])
+                        user = User.objects.create_user(username=form.cleaned_data['username'], email = form.cleaned_data['email'], password = form.cleaned_data['password'])
                         user.save()
-                        account = Account(user=user, name=form.cleaned_data['name']) 
+                        account = Account(user=user, name=form.cleaned_data['name'])
                         account.save()
                         return HttpResponseRedirect('/profile/')
                 else:
@@ -23,3 +25,30 @@ def AccountRegistration(request):
                 form = RegistrationForm()
                 context = {'form': form}
                 return render_to_response('register.html', context, context_instance=RequestContext(request))
+
+def LoginRequest(request):
+        if request.user.is_authenticated():
+                return HttpResponseRedirect('/profile/')
+        if request.method == 'POST':
+                form = LoginForm(request.POST)
+                if form.is_valid():
+                        username = form.cleaned_data['username']
+                        password = form.cleaned_data['password']
+                        account = authenticate(username=username, password=password)
+                        if account is not None:
+                                login(request, account)
+                                return HttpResponseRedirect('/profile/')
+                        else:
+                                return render_to_response('login.html', {'form': form}, context_instance=RequestContext(request))
+                else:
+                        return render_to_response('login.html', {'form': form}, context_instance=RequestContext(request))
+        else:
+                ''' user is not submitting the form, show the login form '''
+                form = LoginForm()
+                context = {'form': form}
+                return render_to_response('login.html', context, context_instance=RequestContext(request))
+
+def LogoutRequest(request):
+        logout(request)
+        return HttpResponseRedirect('/')
+		
