72fdd3629352b79e79a045ea636785038416980e
sjaan <sanderj5@live.nl>
2013-05-30 17:00:05 +0200

    More failing. Soon to be replaced by testrick
    POEP

--- bross/contentblock/admin.py
+++ bross/contentblock/admin.py
@@ -1,18 +1,8 @@
 from django.contrib import admin
 from bross.contentblock.models import Publisher, Author, Book, Content
-from bross.beer.models import Beer, Brewery
 
-class BeerAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ('name',)}
-	list_display = ('name', 'brewery', 'locality')
-	search_fields = ['name']
-
-class BreweryAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Publisher)
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Content)
-admin.site.register(Beer, BeerAdmin)
-admin.site.register(Brewery, BreweryAdmin)
