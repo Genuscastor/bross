4df08533ad0828d260404715a3db1a226df7c685
RickDoesburg <rickdoesburg@gmail.com>
2013-05-30 16:58:56 +0200

    Beers tutorial/module/views added
    The one from Sjaan wasn't working

--- bross/urls.py
+++ bross/urls.py
@@ -16,6 +16,9 @@ urlpatterns = patterns('',
     url(r'^show/(\d)/$', content),
     url(r'^save/$', saveSubmit),
     (r'^register/$', 'bross.account.views.AccountRegistration'),
+    (r'^beers/$', 'bross.beer.views.BeersAll'),
+    (r'^beers/(?P<beerslug>.*)/$', 'bross.beer.views.SpecificBeer'),
+    (r'^brewerys/(?P<breweryslug>.*)/$', 'bross.beer.views.SpecificBrewery'),
 
     #deze aanzetten als import werkt
     url(r'^contact/$', contact),
