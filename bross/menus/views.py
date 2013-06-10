d8fdea4ed48cbf24a6ba884795429a898e5b5fb8
Stefan Pitstra <stefan_pitstra1@hotmail.com>
2013-06-10 14:03:58 +0200

    bla
    asbsd

--- bross/menus/views.py
+++ bross/menus/views.py
@@ -3,7 +3,7 @@ from django.template import RequestContext
 from bross.menus.models import Menu, Parent
 
 def MenusAll(request):
-        parent = Parent.objects.all().order_by('name')
-        menus = Menu.objects.all().order_by('description')
+        parent = Parent.objects.filter(id=3).order_by('name')
+        menus = Menu.objects.filter(parent_id=parent).order_by('parent')
         context = {'menus': menus, 'parent': parent}
         return render_to_response('menusall.html', context, context_instance=RequestContext(request))
